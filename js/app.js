0ffdce848f7b8f6aaa0169437a012f9c7cb24b7d
bylucas <fatman49@me.com>
2024-08-26 12:52:19 +0100

    august24

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
+//Loader
+function spinner() {
+    const spin = document.getElementById('loader-wrapper');
+    setTimeout(function () {
+      spin.classList.add('loaded')
+    }, 100)
+}
+
 //Back to Top
 class Backtotop {
   constructor() {
@@ -51,3 +59,26 @@ popup.addEventListener("click", (e) => {
     popup.style.display = "none"
   }
 })
+
+
+///////////////
+function smoothScroll(target) {
+  const element = document.getElementById(target);
+  if (element) {
+    window.scrollTo({
+      top: element.offsetTop,
+      behavior: 'smooth'
+    });
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  const anchors = document.querySelectorAll('a[href^="#"]');
+  anchors.forEach(function(anchor) {
+    anchor.addEventListener('click', function(event) {
+      event.preventDefault();
+      const target = anchor.getAttribute('href').substring(1);
+      smoothScroll(target);
+    });
+  });
+});
--- js/contact.js
+++ js/contact.js
@@ -1,15 +1,32 @@
 //form
 
-//import { initializeApp } from "firebase/app"
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBgOheePCJqT_ivlXs_EagQ3wQPGIMpJUg",
+  authDomain: "howardlucas-7fcb0.firebaseapp.com",
+  projectId: "howardlucas-7fcb0",
+  storageBucket: "howardlucas-7fcb0.appspot.com",
+  messagingSenderId: "945745527827",
+  appId: "1:945745527827:web:3e282534ecdd22c66eabe6",
+  measurementId: "G-QEE9Y648VB"
+}
+
+// init firebase
+initializeApp(firebaseConfig)
+
+// init firestore service
+const db = getFirestore()
 
 import {
-  //getFirestore,
   collection,
   addDoc,
   serverTimestamp
 } from "firebase/firestore"
 
-import { db } from "./firebase-config.js"
+//import { db } from "./firebase-config.js"
 
 // collection ref
 const colRef = collection(db, "contacts")
